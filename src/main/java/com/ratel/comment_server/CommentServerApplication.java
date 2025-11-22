6ce00a61fcf5dcb9f92f662a4cd4464ff5f90f7b
Francis Kangaty <franciskangaty@gmail.com>
2025-11-22 21:44:09 +0300

    test commit terminal

---
+++ src/main/java/com/ratel/comment_server/CommentServerApplication.java
@@ -0,0 +1,13 @@
+package com.ratel.comment_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratel/comment_server/CommentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratel.comment_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
